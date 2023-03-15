5910df2aa6ea8cf9e0e3c5f4e2b103d42a1e5aaa
aliberkayberber <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>
2023-03-15 21:26:29 +0300

    Project created

---
+++ FoursquareClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoursquareClone
+//
+//  Created by Ali Berkay BERBER on 15.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
