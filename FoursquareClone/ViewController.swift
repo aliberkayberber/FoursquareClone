de485fa2cac0d55d7727c22dd80e4111a314a180
aliberkayberber <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>
2023-03-20 22:22:24 +0300

    Developed Login VC

--- FoursquareClone/ViewController.swift
+++ FoursquareClone/ViewController.swift
@@ -7,13 +7,57 @@
 
 import UIKit
 import Parse
+
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var userNameText: UITextField!
+    
+    @IBOutlet weak var passwordText: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
+    @IBAction func signInClicked(_ sender: Any) {
+        
+        if userNameText.text != "" && passwordText.text != "" {
+            PFUser.logInWithUsername(inBackground: userNameText.text!, password: passwordText.text!) { user, error in
+                if error != nil {
+                    self.makeAlert(titleInput: "Error", massageInput: error?.localizedDescription ?? "Error")
+                } else {
+                    self.performSegue(withIdentifier: "toPlacesVC", sender: nil)
+                }
+            }
+        } else {
+            makeAlert(titleInput: "Error", massageInput: "Username / Password ??")
+        }
+        
+    }
+    
+    @IBAction func signUpClicked(_ sender: Any) {
+        if userNameText.text != "" && passwordText.text != "" {
+            let user = PFUser()
+            user.username = userNameText.text!
+            user.password = passwordText.text!
+            user.signUpInBackground { success, error in
+                if error != nil {
+                    self.makeAlert(titleInput: "Error", massageInput: error?.localizedDescription ?? "Error!")
+                } else {
+                    self.performSegue(withIdentifier: "toPlacesVC", sender: nil)
+                }
+            }
+        } else {
+            makeAlert(titleInput: "Error", massageInput: "Username / Password ??")
+        }
+    }
+    func makeAlert(titleInput: String, massageInput: String) {
+        let alert = UIAlertController(title: titleInput, message: massageInput, preferredStyle: UIAlertController.Style.alert)
+        let action = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+        alert.addAction(action)
+        self.present(alert, animated: true)
+    }
 }
 
